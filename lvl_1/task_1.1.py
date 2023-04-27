9dfff024bc41302baaa30e4382bddb5cc03ed796
Ole4kabelousova <129664704+Ole4kabelousova@users.noreply.github.com>
2023-04-27 13:29:56 +0300

    Add files via upload

--- lvl_1/task_1.1.py
+++ lvl_1/task_1.1.py
@@ -6,3 +6,8 @@ my_favorite_songs = 'Waste a Moment, Staying\' Alive, A Sorta Fairytale, Start M
 
 # Выведите на консоль с помощью индексации строки, последовательно: первый трек, последний, второй, второй с конца
 # Нельзя переопределять my_favorite_songs и запятая не должна выводиться.
+
+print(my_favorite_songs[:30])
+print(my_favorite_songs[-13:])
+print(my_favorite_songs[34:49])
+print(my_favorite_songs[-26:-15])
